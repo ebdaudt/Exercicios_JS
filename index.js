3056b7dd0f3e1a8e9535306817bba9968735af33
EDUARDO BALBINOT DAUDT <04714312065@senacrs.edu.br>
2024-03-05 11:49:29 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// comando para exibir mensagem
+console.log("O INTER vai ser campeão e o gremio não")
+
+const num1 = 20;
+const num2 = 654;
+
+//soma
+let soma = num1 + num2
+
+console.log(`O valor da soma é ${soma}`);
